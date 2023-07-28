5403b228e1df44abd0960f903ce14123a45bebe6
Acunnia <carlosacunagg@gmail.com>
2023-07-28 21:31:07 +0200

    Front para topics

--- client/src/containers/Topic.js
+++ client/src/containers/Topic.js
@@ -1,12 +1,29 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Row, Col, Typography, Form, Input, Button, Modal, Spin } from 'antd';
+import {useSearchParams} from "react-router-dom";
+import api from "../utils/api";
 
 // antd
 const { Title, Text } = Typography;
 const { TextArea } = Input;
 
 const Topic = props => {
+    const [contentLoading, setContentLoading] = useState(true);
+    const [topic, setTopic] = useState({})
+    const [searchParams, setSearchParams] = useSearchParams();
+    const id = searchParams.get("id")
 
+    useEffect(() => {
+        api.get(`/topics${id}`).then(result => {
+            setTopic(result.data)
+            setContentLoading(false)
+        }).catch(err => {
+            Modal.error({
+                title: 'An error ocurred',
+                content: err.message,
+            })
+        })
+    }, [id])
 
     return (
         <div>
