7e947daf4151c9df76907a2b89a5829ab7777960
Acunnia <carlosacunagg@gmail.com>
2023-08-12 19:10:38 +0200

    Corregida tabla de topics

--- client/src/containers/TopicList.js
+++ client/src/containers/TopicList.js
@@ -1,10 +1,13 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {Link, useSearchParams, useLocation, useNavigate} from "react-router-dom";
-import {Col, Skeleton, Modal, Button, Pagination} from "antd";
+import {Skeleton, Modal, Button, Pagination, Table, Typography } from "antd";
 import api from "../utils/api";
 import {BulbOutlined, FormOutlined} from "@ant-design/icons";
 import SubCategory from "../components/SubCategory";
 import {AuthContext} from "../utils/auth";
+import './styles/Forum.styles.css'
+
+const { Text } = Typography;
 
 const TopicList = props => {
     const navigate = useNavigate()
@@ -29,7 +32,6 @@ const TopicList = props => {
         if (isSubcategory) {
             api.get(`/subcategories/topics?id=${id}`)
                 .then((result) => {
-                    console.log(result)
                     setSubcategories([])
                     setTopics(result.data.topics);
                     setUbication(result.data.name) // Just initial
@@ -102,6 +104,46 @@ const TopicList = props => {
 
     };
 
+    const columns = [
+        {
+            title: 'Topic',
+            dataIndex: 'title',
+            key: 'title',
+            render: (text, topic) => (
+                <div>
+                    <Text strong style={{ fontSize: '18px' }}>
+                        {topic.title + "  -  "}
+                    </Text>
+                    <Text type="secondary" style={{ fontSize: '14px' }}>
+                         {topic.description}
+                    </Text>
+                </div>
+            ),
+        },
+        {
+            title: 'Posts',
+            dataIndex: 'replies',
+            render: (replies) => replies.length,
+        },
+        {
+            title: 'Last Update',
+            dataIndex: 'lastreply.postDate',
+            key: 'lastreply.postDate',
+            render: (text, topic) => {
+                const postDate = new Date(topic.lastreply.postDate);
+                return (
+                    <span>
+            {`${postDate.toLocaleTimeString()} ${postDate.toLocaleDateString()}`}
+          </span>
+                );
+            },
+        },
+    ];
+
+    function rowClick() {
+        console.log("awdas")
+    }
+
     return (
         <div>
             {contentLoading ? (
@@ -129,75 +171,23 @@ const TopicList = props => {
                 )}
             </div>
             <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>
-                                <BulbOutlined /> Topic
-                            </th>
-                            <th>
-                                Posts
-                            </th>
-                            <th>
-                                Last update
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td colSpan="3">
-                            <div>
-                                {!contentLoading && topics.length > 0 && pages.totalPages > 0 && (
-                                    <Pagination
-                                        defaultCurrent={pages.currentPage}
-                                        total={pages.totalPages}
-                                        showSizeChanger={false}
-                                        onChange={fetchPage}
-                                    />
-                                )}
-                                {state.isAuthenticated && (
-                                    <Link
-                                        to={`/newtopic?id=${id}`}
-                                    >
-                                        <Button type="primary" icon={<FormOutlined />}>
-                                            Create new topic
-                                        </Button>
-                                    </Link>
-                                )}
-                            </div>
-                        </td>
-                    </tr>
-                    {contentLoading ? (
-                        <div>
-                            <Skeleton active />
-                            <Skeleton active />
-                            <Skeleton active />
-                        </div>
-                    ) : (
-                        topics.map(topic => {
-                            return (
-                                <tr key={topic._id}>
-                                    <td>
-                                        <div>
-                                            <Link to={`/topic?id=${topic._id}`}>
-                                                <h3>
-                                                    {topic.title}
-                                                </h3>
-                                                <p>
-                                                    {topic.description}
-                                                </p>
-                                            </Link>
-                                        </div>
-                                    </td>
-                                    <td>{topic.replies.length}</td>
-                                    <td>{`${String(new Date(topic.lastreply.postDate).getHours()).padStart(2, '0')}:${String(new Date(topic.lastreply.postDate).getMinutes()).padStart(2, '0')} ${String(new Date(topic.lastreply.postDate).getDate()).padStart(2, '0')}/${String(new Date(topic.lastreply.postDate).getMonth() + 1).padStart(2, '0')}/${new Date(topic.lastreply.postDate).getFullYear()}`}</td>
-
-                                </tr>
-                            )
-                        })
-                    )}
-                    </tbody>
-                </table>
+                <Table
+                    columns={columns}
+                    dataSource={topics}
+                    rowKey={(topic) => topic._id}
+                    pagination={false}
+                    loading={contentLoading}
+                    onRow={(topic) => ({
+                        onClick: () => navigate(`/topic?id=${topic._id}`)
+                    })}
+                    className="hoverable-table"
+                />
+                <Pagination
+                    current={pages.currentPage}
+                    total={pages.totalPages}
+                    showSizeChanger={false}
+                    onChange={fetchPage}
+                />
 
                 <Link
                     to={`/newsubcat?id=${id}`}
